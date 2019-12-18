c5cddfd8eb30b3ea45a3d555835f7e4d50ffd74f
Mathilde <mathilde.roussier@laplateforme.io>
2019-12-18 15:36:23 +0100

    MAJ html -> Ajout formulaire profil et reservation-form

--- profil.php
+++ profil.php
@@ -14,6 +14,32 @@
 
     <main>
 
+        <form method='POST' action='profil.php'>
+
+            <article>
+                <label> Login </label>
+                <input type="text" name="login" />
+            </article>
+
+            <article>
+                <label> Mot de passe actuel </label>
+                <input type="password" name="old_password" required />
+            </article>
+
+            <article>
+                <label> Nouveau mot de passe </label>
+                <input type="password" name="password" />
+            </article>
+
+            <article>
+                <label> Confirmation du nouveau mot de passe </label>
+                <input type="password" name="password_conf" />
+            </article>
+
+            <input type="submit" name='profil' value='Modifier' />
+
+        </form>
+
     </main>
 
     <?php include 'include/footer.php' ?>
--- reservation-form.php
+++ reservation-form.php
@@ -14,10 +14,38 @@
 
     <main>
 
+        <form method='POST' action='reservation-form.php'>
+
+            <article>
+                <label> Titre </label>
+                <input type="text" name="titre" required />
+            </article>
+
+            <article>
+                <label> Description </label>
+                <textarea name='description'></textarea>
+            </article>
+
+            <article>
+                <label> Début </label>
+                <input type='date' name='datedebut' required />
+                <input type='time' name='timedebut' required />
+            </article>
+
+            <article>
+                <label> Fin </label>
+                <input type='date' name='datefin' required />
+                <input type='time' name='timefin' required />
+            </article>
+
+            <input type="submit" name='reservation' value='Réserver' />
+
+        </form>
+
     </main>
 
     <?php include 'include/footer.php' ?>
-    
+
 </body>
 
 </html>
