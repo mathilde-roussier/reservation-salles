19f8f26487b15fab1c522ea80ff845d843e25eb6
Mathilde <mathilde.roussier@laplateforme.io>
2019-12-22 13:52:40 +0100

    MAJ -> Traitement date --> correspondance creneau

--- include/traitement-tableau.php
+++ include/traitement-tableau.php
@@ -3,20 +3,30 @@
 // recup liste evenements
 $connexion = mysqli_connect('localhost','root','','reservationsalles');
 $check_event = "SELECT * FROM reservations JOIN utilisateurs ON reservations.id_utilisateur = utilisateurs.id";
-$query_check_event = mysqli_query($connexion,$check_event);
-$resultat_check_event = mysqli_fetch_all($query_check_event,MYSQLI_ASSOC);
-// var_dump($resultat_check_event);
+$resultat_event = mysqli_query($connexion,$check_event);
 
-for ($i = 8; $i <= 18; $i++)
+for ($h = 8; $h <= 18; $h++)
 {
-    echo "<tr><td>". $i ."</td>";
+    echo "<tr><td>". $h ."</td>";
 
-    for ($j = 0; $j <= 4; $j++) 
+    for ($j = 1; $j <= 5; $j++) 
     {
-        echo "<td> <a href='reservation-form.php'> Réserver ce créneau </a> </td>";
+        $creneau = $j."-".$h;
+        echo "<td> <a href='reservation-form.php'> Réserver ce créneau ".$creneau."</a> </td>";
     }
 
     echo "</tr>";
+}
+
+foreach ($resultat_event as $donnees)
+{
+    var_dump($donnees);
+
+    // Récupération jour et heure de la date séparément:
+    $date = explode(' ',$donnees['debut']);
+    // Jour
+    list($annee,$mois,$jour) = explode('-',$date[0]);
+    $jour = date('N',mktime(0,0,0,$mois,$jour,$annee));
 }
 
 ?>
